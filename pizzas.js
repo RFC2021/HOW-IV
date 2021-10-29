cfd208893b824f776314909280c1a53070e329ad
RFC2021 <81125968+RFC2021@users.noreply.github.com>
2021-10-29 15:51:44 -0300

    Add files via upload

---
+++ pizzas.js
@@ -0,0 +1,6 @@
+let pizzaJson = [
+    {id:1, name:'Brocolis Com Queijo', img:'pizza.png', price:20.19, sizes:['100g', '530g', '860g'], description:'pizza'},
+    {id:2, name:'Portuguesa', img:'pizza2.png', price:18.00, sizes:['320g', '530g', '860g'], description:'pizza'},
+    {id:3, name:'La da Casa', img:'pizza3.png', price:17.45, sizes:['320g', '530g', '860g'], description:'pizza'},
+    
+];
